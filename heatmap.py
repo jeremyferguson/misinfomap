a9f094d475e58aa595617faafe70a4611a48feb5
dwolfe884 <43447026+dwolfe884@users.noreply.github.com>
2020-04-25 22:36:35 -0500

    Add files via upload

---
+++ heatmap.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import json
+
+with open('C:\\Users\\David\\Desktop\\Hackathon\\covidstatesdaily.json') as f:
+    #data = json.load(f)
+    pandaData = pd.read_json(f)
+
+fig = go.Figure(data=[go.Choropleth(
+    locations=pandaData[:50]['state'], # Spatial coordinates
+    z = pandaData[:50]['positive'], # Data to be color-coded
+    locationmode = 'USA-states', # set of locations match entries in `locations`
+    colorscale = 'Reds',
+    colorbar_title = "Confirmed Cases",
+    
+)])
+dataFramArr = []
+mydata = pd.DataFrame(columns=['date','state','positive'])
+currDate = ""
+for index, row in pandaData.iterrows():
+    if(currDate == ""):
+        currDate = row['date']
+    if(row['date'] != currDate):
+        currDate = row['date']
+        dataFramArr.append(mydata)
+        mydata = pd.DataFrame(columns=['date','state','positive'])
+    mydata.loc[index] = [row['date'],row['state'],row['positive']]
+    #print(dataFramArr)
+    
+for frame in dataFramArr:
+    fig.add_trace(go.Choropleth(
+    locations=frame['state'], # Spatial coordinates
+    z = frame['positive'], # Data to be color-coded
+    locationmode = 'USA-states', # set of locations match entries in `locations`
+    colorscale = 'Reds',
+    colorbar_title = "Confirmed Cases",   
+    ))
+steps = []
+for i in range(len(dataFramArr)):
+    step = dict(method='restyle',
+                args=['visible', [False] * int((len(dataFramArr)))],
+                label='Date {}'.format(i + 20200122))
+    step['args'][1][i] = True
+    steps.append(step)
+#print(steps)
+sliders = [dict(active=0,
+                pad={"t": 1},
+                steps=steps)]    
+layout = dict(geo=dict(scope='usa',
+                       projection={'type': 'albers usa'}),
+              sliders=sliders)
+
+fig.update_layout(layout)
+
+fig.show()
